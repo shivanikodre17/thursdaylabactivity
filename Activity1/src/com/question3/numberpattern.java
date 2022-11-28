ee4d3df82ad5942dec214ace45d2b57b9a6347b9
abc <abc@DESKTOP-K2V9QDB>
2022-11-28 11:23:53 +0530

    lab project

---
+++ Activity1/src/com/question3/numberpattern.java
@@ -0,0 +1,19 @@
+package com.question3;
+
+public class numberpattern {
+
+	public static void main(String[] args) {
+		int rows = 5, number = 1;
+
+	    for(int i = 1; i <= rows; i++) {
+
+	      for(int j = 1; j <= i; j++) {
+	        System.out.print(number + " ");
+	        ++number;
+	      }
+
+	      System.out.println();
+	    }
+	}
+
+}
